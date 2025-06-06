0863cdae3f9678a3fc1a2ac5612f273d9a28646a
maru <maru@lithium-dev.xyz>
2025-06-06 18:24:54 -0400

    Update ex.py

--- ex.py
+++ ex.py
@@ -1,14 +1,15 @@
 from stdio import *
 
 """
-lp > "str\\n"                # basically printf, low level sys stdout requires newlines  
-p > "str"                 # lp but with auto-newline   
-li > "str"                # low level input with sys stdin  
-hi > "str"                # input() wrapper  
-hg > ''                   # args are ignored  
-b64d > "base64string"     # returns decoded str  
-cmd > "sh command"        # os.system wrapper  
-pya > "PYTHON_CMD args"   # just a workaround for parenthesis cus i think its funny to not use them  
+lp < "str\\n"             # basically printf, low level sys stdout requires newlines  
+p < "str"                 # lp but with auto-newline   
+li < "str"                # low level input with sys stdin  
+i < "str"                 # li but with auto newline
+hg < ''                   # args are ignored  getch's input Linux and windows(no darwin)
+b64d < "base64string"     # returns decoded str  
+cmd < "sh command"        # os.system wrapper  
+pya < "PYTHON_CMD args"   # just a workaround for parenthesis cus i think its funny to not use them  
+exe < var OR "python code"# exec wrapper
 """
 
 a='hi'
